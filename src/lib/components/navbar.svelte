5982e41b6551ec8a662f440e54fcceaa182ad876
Mohammad <reiaqua0@gmail.com>
2024-11-13 14:01:03 +0300

    Add navbar

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,21 @@
+<div class="fixed top-0 flex w-svw items-center justify-center p-4">
+	<nav class="flex h-16 w-10/12 max-w-screen-lg items-center rounded-lg bg-background p-4">
+		<div>
+			<a href="/" aria-label="Home">
+				<svg
+					width="95"
+					height="25"
+					viewBox="0 0 95 25"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						d="M5 5V0H15V5H5ZM5 25H0V5H5V10H15V5H20V25H15V15H5V25ZM30 5V0H40V5H30ZM30 20H25V5H30V20ZM40 15V5H45V15H40ZM40 20H35V15H40V20ZM35 20V25H30V20H35ZM40 25V20H45V25H40ZM55 20H50V0H55V20ZM70 20H65V0H70V20ZM65 20V25H55V20H65ZM80 5V0H90V5H80ZM80 25H75V5H80V10H90V5H95V25H90V15H80V25Z"
+						fill="#05696B"
+					/>
+				</svg>
+			</a>
+		</div>
+	</nav>
+</div>
+
