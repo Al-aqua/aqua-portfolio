240df333e5f23e0e380d4fd87452e2eb131506a4
Mohammad <reiaqua0@gmail.com>
2024-11-13 13:52:02 +0300

    Add color scheme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,13 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
+		colors: {
+			text: '#f5f9fa',
+			background: '#0A0D0D',
+			primary: '#05696B',
+			secondary: '#4B515C',
+			accent: '#878A9D'
+		},
 		extend: {}
 	},
 
